5a5536a9f1dc8b181689105a976be3b4e1a874c7
Archit <architrathod77@gmail.com>
2023-03-10 20:46:15 +0530

    Initial

---
+++ components/providers.tsx
@@ -0,0 +1,26 @@
+"use client";
+import * as React from "react";
+import AppProvider from "../context/AppContext";
+import { AnimatePresence, motion } from "framer-motion";
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <AppProvider>
+            <AnimatePresence>
+                <motion.div
+                    initial={{ opacity: 0, y: 15 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: 15 }}
+                    transition={{
+                        delay: 0.5,
+                        ease: "easeInOut",
+                    }}
+                >
+                    {children}
+                </motion.div>
+            </AnimatePresence>
+        </AppProvider>
+    );
+};
+
+export default Providers;
